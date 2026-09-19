9d652fcd18ab8424fc2533a355f3469426880367
Dikshit Sharma <noreply@feedback.github.com>
2026-09-19 15:27:55 +0530

    add jwt token

--- app/config/config.py
+++ app/config/config.py
@@ -5,3 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+
+JWT_ALGORITHM = "HS256"
+
+JWT_ACCESS_TOKEN_EXPIRE_MINUTES  = "60"
